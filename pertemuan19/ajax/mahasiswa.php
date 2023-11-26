5e924e5ed830609211c892d52633f4c6936c08d8
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2023-11-26 17:16:29 +0700

    feat: Add Live Search

---
+++ pertemuan19/ajax/mahasiswa.php
@@ -0,0 +1,32 @@
+<?php
+require '../functions.php';
+$mahasiswa = cari($_GET['keyword']);
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NPM</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+    <?php $i = 1; ?>
+    <?php foreach ($mahasiswa as $row) : ?>
+        <tr>
+            <td><?= $i; ?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> ||
+                <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"]; ?>" width="50" alt="<?= $row["nama"]; ?>"></td>
+            <td><?= $row["npm"]; ?></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["email"]; ?></td>
+            <td><?= $row["jurusan"]; ?></td>
+        </tr>
+        <?php $i++; ?>
+    <?php endforeach; ?>
+</table>
